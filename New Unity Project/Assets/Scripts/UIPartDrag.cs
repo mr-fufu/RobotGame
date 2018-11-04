93e9cc2df6138d192d08d71eba2c71a501d32c78
Unknown <monkeymanp8@gmail.com>
2018-11-03 22:39:57 -0500

    VSBrokenAndSwitchingComputers
    idk I think I broke VS im gon stop here briefly

---
+++ New Unity Project/Assets/Scripts/UIPartDrag.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIPartDrag : MonoBehaviour {
+
+    public Transform init_position;
+
+
+	// Use this for initialization
+	void Start () {
+        init_position = gameObject.Transform;
+	}
+	
+    void Update()
+    {
+        Input.GetMouseButtonDown(0);
+    }
+
+	// Update is called once per frame
+	void OnMouseDrag () {
+		
+	}
+}
